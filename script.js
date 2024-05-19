ce53d1218dfe0a4de7345e45e28ed1cfcad35222
Jakub Jarosz <tar.jakubjarosz@gmail.com>
2024-05-19 13:40:36 +0200

    added percent counting part for guessing

--- script.js
+++ script.js
@@ -16,6 +16,12 @@ const inputBlue = document.createElement('input');
 const buttonSend = document.createElement('button');
 buttonSend.innerHTML = 'Check!';
 
+//
+let points = 0;
+let result = 0;
+let randNumberRed = 0;
+let randNumberGreen = 0;
+let randNumberBlue = 0;
 //Adding created objects to game
 game.appendChild(inputRed);
 game.appendChild(document.createElement('br'));
@@ -51,6 +57,10 @@ generateNewColor();
 
 function sendRGB(){
 if(inputRed.value&&inputGreen.value&&inputBlue.value){
+    result = (1-(Math.abs(inputRed.value-randNumberRed)/255+
+    Math.abs(inputGreen.value-randNumberGreen)/255+
+    Math.abs(inputBlue.value-randNumberBlue)/255))*100;
+    console.log('Your are '+ result +'% correct.');
     console.log('Your RGB: (' +
         inputRed.value + ', ' +
         inputGreen.value + ', ' +
@@ -70,9 +80,9 @@ textBox.innerHTML = "You need to type three numbers 0-255!"
 
 function generateNewColor(){
 
-let randNumberRed = Math.floor(Math.random()*255);
-let randNumberGreen = Math.floor(Math.random()*255);
-let randNumberBlue = Math.floor(Math.random()*255);
+randNumberRed = Math.floor(Math.random()*255);
+randNumberGreen = Math.floor(Math.random()*255);
+randNumberBlue = Math.floor(Math.random()*255);
 
 console.log('Generated RGB: (' + randNumberRed + ', '+randNumberGreen + ', '+randNumberBlue+ ')');
 colorSquare.style.backgroundColor = 'rgb('+randNumberRed+', '+randNumberGreen+', '+randNumberBlue+')';
