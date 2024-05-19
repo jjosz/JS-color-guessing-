204dee9b0c74bf89bd82d9b94a459ba17562f310
Jakub Jarosz <tar.jakubjarosz@gmail.com>
2024-05-19 12:53:41 +0200

    added eventListener for button

--- script.js
+++ script.js
@@ -9,11 +9,19 @@ const inputRed = document.createElement('input');
 const inputGreen = document.createElement('input');
 const inputBlue = document.createElement('input');
 
+//Creating button for sending chosen numbers
+const buttonSend = document.createElement('button');
+buttonSend.innerHTML = 'Check!';
+
+//Adding created objects to game
 game.appendChild(inputRed);
 game.appendChild(document.createElement('br'));
 game.appendChild(inputGreen);
 game.appendChild(document.createElement('br'));
 game.appendChild(inputBlue);
+game.appendChild(document.createElement('br'));
+game.appendChild(buttonSend);
+
 
 Object.assign(inputRed, {
     type: 'text',
@@ -30,11 +38,21 @@ Object.assign(inputBlue, {
     placeholder: 'Blue'
 });
 
+buttonSend.addEventListener('click',sendRGB);
+
 
 colorSquare.style = 'width: 100px; height:100px;';
 colorSquare.style = 'width: 100px; height:100px;';
 
 generateNewColor();
+
+function sendRGB(){
+if(inputRed.value&&inputGreen.value&&inputBlue.value){
+console.log('Your RGB: (' + inputRed.value + ', ' + inputGreen.value + ', ' + inputBlue.value + ')' )
+}
+console.log('-----');
+generateNewColor();
+}
 
 function generateNewColor(){
 
@@ -42,6 +60,6 @@ let randNumberRed = Math.floor(Math.random()*255);
 let randNumberGreen = Math.floor(Math.random()*255);
 let randNumberBlue = Math.floor(Math.random()*255);
 
-console.log(randNumberRed + ', '+randNumberGreen + ', '+randNumberBlue);
+console.log('Generated RGB: (' + randNumberRed + ', '+randNumberGreen + ', '+randNumberBlue+ ')');
 colorSquare.style.backgroundColor = 'rgb('+randNumberRed+', '+randNumberGreen+', '+randNumberBlue+')';
 }
